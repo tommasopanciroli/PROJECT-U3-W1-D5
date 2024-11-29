966bacd725c47bc1fd33c7573e20ac99639ec875
tommaso.panciroli <tommypanci@gmail.com>
2024-11-29 14:24:20 +0100

    updated navbar

--- src/components/NetflixNavbar.jsx
+++ src/components/NetflixNavbar.jsx
@@ -4,20 +4,20 @@ import logo from '../assets/img/logo.png'
 
 const NetflixNavbar = (props) => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" data-bs-theme="dark">
+    <Navbar collapseOnSelect expand="lg">
       <Container fluid={props.isFluid || true}>
         <Navbar.Brand href="#home">
           {' '}
           <img src={logo} alt="logo" width={100} />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className='bg-light'/>
+        <Navbar.Collapse id="basic-navbar-nav" >
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Tv Shows</Nav.Link>
-            <Nav.Link href="#link">Movies</Nav.Link>
-            <Nav.Link href="#link">Recently Added</Nav.Link>
-            <Nav.Link href="#link">My List</Nav.Link>
+            <Nav.Link className='text-white' href="#home">Home</Nav.Link>
+            <Nav.Link className='text-white' href="#link">Tv Shows</Nav.Link>
+            <Nav.Link className='text-white' href="#link">Movies</Nav.Link>
+            <Nav.Link className='text-white' href="#link">Recently Added</Nav.Link>
+            <Nav.Link className='text-white' href="#link">My List</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
